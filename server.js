e60bb3facc363491ab9f7bc94f8a33f62e090bc4
Alex Z. Yang <zya@ucsd.edu>
2016-10-27 16:00:13 -0700

    chaneged listening port

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 var express = require('express');
 var app = express();
 app.use('/', express.static(__dirname + '/static'));
-app.listen(3000, function() { console.log('listening')});
+app.listen(process.env.PORT || 5000, function() { console.log('listening')});
